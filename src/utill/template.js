1ec53b0dec84854dc1b943e69549742d78996afe
jangot <pulin.pavel@gmail.com>
2023-05-06 16:03:54 +0200

    move templates

--- src/utill/template.js
+++ src/utill/template.js
@@ -3,7 +3,7 @@ const { template } = require('lodash');
 
 module.exports = {
     render: async (name, params = {}) => {
-        const html = await readFile(`${__dirname }/templates/${name}.html`);
+        const html = await readFile(`templates/${name}.html`);
         console.log(html)
         return template(html.toString())(params);
     }
